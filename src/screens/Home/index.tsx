5198e22cccbf5e470871296a67582c65095b73c4
Joao Suza <joao.souza3157@gmail.com>
2022-03-30 13:45:26 -0300

    configurando para typescript

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+import{ styles } from './styles';
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your teste!</Text>
+    </View>
+  );
+}
+
+
